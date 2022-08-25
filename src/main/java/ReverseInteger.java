f8383068bea61abe35f25121a3df55798dbe699d
Anmol Pal <anmolpal@Anmols-Air.lan>
2022-08-25 08:10:32 -0400

    L-C

---
+++ src/main/java/ReverseInteger.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/reverse-integer/
+public class ReverseInteger {
+    public static void main(String[] args) {
+    ReverseInteger reverseInteger= new ReverseInteger();
+    int x=-1534236469;
+//    int x=-120;
+    System.out.println(reverseInteger.reverse(x));
+    }
+    public int reverse(int x) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        int sign=1, reversed=0, tail;
+        if(x<0){
+            sign=-1;
+        }
+        while(x!=0){
+            if(reversed>Integer.MAX_VALUE/10 || reversed<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            tail=x%10;
+            reversed=tail+ reversed*10;
+            x=x/10;
+            }
+            System.out.println("Rev: "+reversed+" x: "+x);
+        return reversed;
+
+    }
+
+    }
+
+
+
---
+++ src/main/java/RomantoInteger.java
@@ -0,0 +1,89 @@
+//https://leetcode.com/problems/roman-to-integer/
+public class RomantoInteger {
+    public static void  main(String[] args) {
+        RomantoInteger romantoInteger = new RomantoInteger();
+//        String s="MCMXCIV"; //1994
+        String s="III"; //3
+        System.out.println(romantoInteger.romanToInt(s));
+//        System.out.println(romantoInteger.romanToInt2(s));
+
+    }
+    public int romanToInt(String s) {
+        char c;
+        int counter=0;
+        for(int i=0; i<s.length(); i++){
+            switch (s.charAt(i)){
+                case 'I':
+                    if(i<s.length()-1 && (s.charAt(i+1) == 'V' || s.charAt(i+1)=='X' ) ){
+                        switch (s.charAt(i+1)){
+                            case 'V':
+                                counter=counter+4;
+                                i++;
+                                break;
+                            case 'X':
+                                counter=counter+9;
+                                i++;
+                                break;
+                        }
+                    }
+                    else {
+                        counter = counter + 1;
+                    }
+                    break;
+                case 'V':
+                    counter=counter+5;
+                    break;
+                case 'X':
+                    if(i<s.length()-1 && (s.charAt(i+1) == 'L' || s.charAt(i+1)=='C' ) ){
+                        switch (s.charAt(i+1)){
+                            case 'L':
+                                counter=counter+40;
+                                i++;
+                                break;
+                            case 'C':
+                                counter=counter+90;
+                                i++;
+                                break;
+                        }}
+                    else {
+                        counter=counter+10;
+                    }
+                    break;
+                case 'L':
+                    counter=counter+50;
+                    break;
+                case 'C':
+                    if(i<s.length()-1 && (s.charAt(i+1) == 'D' || s.charAt(i+1)=='M' ) ){
+                        switch (s.charAt(i+1)){
+                            case 'D':
+                                counter=counter+400;
+                                i++;
+                                break;
+                            case 'M':
+                                counter=counter+900;
+                                i++;
+                                break;
+                        }}
+                    else {
+                        counter=counter+100;
+                    }
+                    break;
+                case 'D':
+                    counter=counter+500;
+                    break;
+                case 'M':
+                    counter=counter+1000;
+                    break;
+
+            }
+        }
+        return counter;
+    }
+//    public int romanToInt2(String s) {
+//    int counter=0;
+//    Map<Character,Integer=>
+//    return counter;
+//    }
+
+
+}
