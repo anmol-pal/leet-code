3df00af59d0c64811f5686bcf8e6dc0ebb28b595
Anmol Pal <anmolpal@Anmols-Air.lan>
2022-09-04 12:17:05 -0400

    Update

--- src/main/java/gsprep/NonRepeatingChar.java
+++ src/main/java/gsprep/NonRepeatingChar.java
@@ -1,8 +1,45 @@
 package gsprep;
+
+import java.util.HashMap;
+import java.util.Map;
+
 //https://www.geeksforgeeks.org/given-a-string-find-its-first-non-repeating-character/
+class CountIndex{
+    int count, index;
+    public CountIndex(int index){
+        this.count= 1;
+        this.index=index;
+    }
+    public CountIndex(){
+        this.count++;
+    }
+}
 public class NonRepeatingChar {
     public static void main(String[] args) {
+        String s= "geeee";
+        NonRepeatingChar nonRepeatingChar=new NonRepeatingChar();
+        nonRepeatingChar.firstNonRepeating(s);
 
     }
-    
+
+    public int firstNonRepeating(String s){
+
+        HashMap<Character,CountIndex> map= new HashMap();
+        System.out.println(map);
+        for(int i=0; i<s.length()-1;i++){
+            if(map.containsKey(s.charAt(i))){
+                    map.put(s.charAt(i),map.get(s.charAt(i)).count);
+            }
+            else{
+                map.put(s.charAt(i),new CountIndex(i));
+            }
+        }
+        for( Map.Entry<Character, CountIndex> entry : map.entrySet() ){
+        int c= entry.getValue().count;
+        int ind= entry.getValue().index;
+        char ci= entry.getKey();
+        System.out.println("Char: "+ci+" Index:"+ind+" Count"+c);
+        }
+        return 0;
+    }
 }
